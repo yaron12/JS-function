4d914bd5515e7a040be7ce9c1a9ce353ca1e1576
yaronbensimon1@gmail.com <026535898>
2016-08-21 23:12:03 +0300

    exercise 4

--- flag.js
+++ flag.js
@@ -1,3 +1,22 @@
 /**
  * @author ybensimx
  */
+function showCountry() {
+				//document.getElementById("country").style.visibility = "visible";
+				var x = document.getElementsByClassName("country");
+				x[0].innerHTML = "israel"; 
+				x[1].innerHTML = "use";
+				x[2].innerHTML = "italy";
+				x[3].innerHTML = "netherlands";
+				x[4].innerHTML = "france";  
+			}
+
+			function hiddenCountry() {
+				//document.getElementById("country").style.visibility = "hidden";
+				var x = document.getElementsByClassName("country");
+				x[0].innerHTML = "";
+				x[1].innerHTML = "";
+				x[2].innerHTML = "";
+				x[3].innerHTML = "";
+				x[4].innerHTML = "";
+			}
